580c64e117c8aaaae5d719f406789487e9f1c367
elshandideptech <elshandi@deptechdigital.com>
2023-10-13 14:45:43 +0700

    feat(migration): employee_take_leaves table

---
+++ database/migrations/2023_10_13_073528_create_employee_take_leaves_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_take_leaves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_employee');
+            $table->foreignId('id_leave');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_take_leaves');
+    }
+};
