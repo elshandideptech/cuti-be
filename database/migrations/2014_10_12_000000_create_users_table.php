201342199105cc514e6fb3f19e1a825c4e458dea
elshandideptech <elshandi@deptechdigital.com>
2023-10-13 10:29:15 +0700

    feat(migration): define attributes users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
