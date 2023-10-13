e8f56a137ed9be94181dc8edea5f201985aaf6ab
elshandideptech <elshandi@deptechdigital.com>
2023-10-13 10:52:06 +0700

    feat(migration): define attributes laves table

---
+++ database/migrations/2023_10_13_033047_create_leaves_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('id_employee')->references('id')->on('employees');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('reason', 30);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
