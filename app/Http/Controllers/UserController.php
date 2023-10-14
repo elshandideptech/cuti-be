74c73fdc067583ccb2db8f9f74f8b18e6f1a9e60
elshandideptech <elshandi@deptechdigital.com>
2023-10-14 22:22:49 +0700

    feat(api): update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,4 +47,38 @@ class UserController extends Controller
             return ApiResponse::errorResponse('Failed Insert User', $th->getMessage(), 500);
         }
     }
+
+    /**
+     * fungsi untuk mengubah data user
+     * * @author Elshandi Septiawan <elshandi@deptechdigital.com> 
+     * @param $first_nama First Name of User (required, max 20 characters)
+     * @param $last_nama Last Name of User (required, max 20 characters)
+     * @param $email Email of User (required, max 50 characters)
+     * @return void success message and data inserted
+     * created at October 14, 2023
+     */
+    public function update(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:20',
+            'last_name' => 'required|string|max:20',
+            'email' => 'required|email|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        try {
+            $user = User::find($id);
+            $user->update([
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+            ]);
+            
+            return ApiResponse::successResponse($user, 'Success Update User');
+        } catch (\Throwable $th) {
+            return ApiResponse::errorResponse('Failed Update User', $th->getMessage(), 500);
+        }
+    }
 }
