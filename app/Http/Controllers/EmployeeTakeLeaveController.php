a0f19013b0b4c637e07431ad00e79225aa6d9b1d
elshandideptech <elshandi@deptechdigital.com>
2023-10-15 21:16:03 +0700

    feat(api): update emloyee take leave

--- app/Http/Controllers/EmployeeTakeLeaveController.php
+++ app/Http/Controllers/EmployeeTakeLeaveController.php
@@ -39,4 +39,36 @@ class EmployeeTakeLeaveController extends Controller
             return ApiResponse::errorResponse('Failed Insert Employee Take Leave', $th->getMessage(), 500);
         }
     }
+
+    /**
+     * fungsi untuk mengubah data karyawan mengambil cuti
+     * * @author Elshandi Septiawan <elshandi@deptechdigital.com> 
+     * @param $id_employee employee's id who take leave (required, integer)
+     * @param $id_leave leave's id will be taken (required, integer)
+     * @param $start_date date when employee start leave (required, date, date format:YYYY-MM-DD)
+     * @param $end_date date when employee end leave (required, date, date format:YYYY-MM-DD, after start date)
+     * @return void success message and data updated
+     * created at October 15, 2023
+     */
+    public function update(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'id_employee' => 'required|integer',
+            'id_leave' => 'required|integer',
+            'start_date' => 'required|date|date_format:Y-m-d',
+            'end_date' => 'required|date|date_format:Y-m-d|after:start_date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        try {
+            $employee_take_leave = EmployeeTakeLeave::find($id);
+            $employee_take_leave->update($validator->validated());
+            
+            return ApiResponse::successResponse($employee_take_leave, 'Success Update Employee Take Leave');
+        } catch (\Throwable $th) {
+            return ApiResponse::errorResponse('Failed Update Employee Take Leave', $th->getMessage(), 500);
+        }
+    }
 }
